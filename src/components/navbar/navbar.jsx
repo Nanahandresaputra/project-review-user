fc134b8536910c8e43d1375ba74263720f6d3635
Nanahandresaputra <nanaggmu@gmail.com>
2023-08-15 07:11:15 +0700

    progress 7.11am 15/8/23

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -18,8 +18,8 @@ const Navbar = () => {
   const navigate = useNavigate();
 
   return (
-    <section className="navbar bg-sky-500 text-white flex justify-around px-5 fixed top-0 z-20">
-      <div className="flex items-center space-x-5">
+    <section className="navbar bg-sky-500 text-white flex justify-around px-1 md:px-5 fixed top-0 z-20">
+      <div className="flex items-center space-x-0 md:space-x-5">
         <TfiArrowCircleLeft className="hidden lg:block text-3xl hover:cursor-pointer active:scale-125 transition" onClick={() => navigate(-1)} />
         <Link to="/" className=" font-medium md:text-xl lg:text-2xl">
           PT CERBERUS SINERGI DATASISTEM
@@ -27,14 +27,14 @@ const Navbar = () => {
       </div>
       <div className="relative flex flex-col items-center">
         <div className="flex space-x-3 items-center  cursor-pointer" onClick={handleOpen}>
-          {id ? <img src={userData[id].foto} alt="foro user" className="w-16 h-16 rounded-full" /> : <BiSolidUserCircle className="text-4xl md:text-5xl" />}
+          {id ? <img src={userData[id].foto} alt="foro user" className="w-12 h-12 md:w-16 md:h-16 rounded-full" /> : <BiSolidUserCircle className="text-4xl md:text-5xl" />}
           <p className="hidden md:block font-medium md:text-xl lg:text-2xl">{id ? userData[id].nama : null}</p>
         </div>
 
         <div
           className={
             open
-              ? "opacity-100 absolute top-10 md:top-12 lg:top-14 py-2 w-44 flex flex-col items-center bg-sky-500 transition md:text-lg"
+              ? "opacity-100 absolute top-12 md:top-16 py-2 w-44 flex flex-col items-center bg-sky-500 transition md:text-lg"
               : "opacity-0  absolute top-10 md:top-12 lg:top-14 py-2 w-44 flex flex-col items-center bg-sky-500 transition md:text-lg"
           }
         >
